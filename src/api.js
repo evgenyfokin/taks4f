b7c38d23a8e6c3c2aaef5f3ab97001df905851b6
evgenyfokin <fokin_e@inbox.ru>
2023-07-26 00:16:30 +0300

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://chatf-594d6adb216e.herokuapp.com'
 });
 
 instance.interceptors.response.use(
